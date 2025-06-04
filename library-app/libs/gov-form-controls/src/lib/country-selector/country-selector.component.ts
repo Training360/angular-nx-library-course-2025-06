a0526dd41636386fd1ddffb0ee92a9a9f5c0cbdc
cherryApp <j.cserko@gmail.com>
2025-06-04 12:46:16 +0200

    CVA done

--- library-app/libs/gov-form-controls/src/lib/country-selector/country-selector.component.ts
+++ library-app/libs/gov-form-controls/src/lib/country-selector/country-selector.component.ts
@@ -1,6 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { CommonModule, LowerCasePipe } from '@angular/common';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+export interface ICountryCode {
+  code: string;
+  name: string;
+}
 
 @Component({
   selector: 'lib-country-selector',
@@ -16,9 +21,27 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
   ],
 })
 export class CountrySelectorComponent implements ControlValueAccessor {
+  counties = input<ICountryCode[]>([
+    { code: 'HU', name: 'HUN' },
+    { code: 'US', name: 'USA' },
+    { code: 'NL', name: 'NLD' },
+    { code: 'JM', name: 'JAM' },
+    { code: 'SZ', name: 'SWZ' },
+  ]);
+
   selected = signal<string>('');
 
+  disabled = signal<boolean>(false);
+
   private onChanged!: (v: string) => void;
+
+  private onTouched!: () => void;
+
+  selectCountry(code: string): void {
+    this.onTouched();
+    this.selected.set(code);
+    this.onChanged(code);
+  }
 
   writeValue(value: string): void {
     this.selected.set(value);
@@ -28,11 +51,11 @@ export class CountrySelectorComponent implements ControlValueAccessor {
     this.onChanged = fn;
   }
 
-  registerOnTouched(fn: any): void {
-    throw new Error('Method not implemented.');
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
   }
 
   setDisabledState?(isDisabled: boolean): void {
-    throw new Error('Method not implemented.');
+    this.disabled.set(isDisabled);
   }
 }
