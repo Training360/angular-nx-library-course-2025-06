7ea9e17d7082872d9360471510040f2221d592a7
cherryApp <j.cserko@gmail.com>
2025-06-04 12:02:07 +0200

    apply CVA

--- library-app/libs/gov-form-controls/src/lib/country-selector/country-selector.component.ts
+++ library-app/libs/gov-form-controls/src/lib/country-selector/country-selector.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { CommonModule, LowerCasePipe } from '@angular/common';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'lib-country-selector',
-  imports: [CommonModule],
+  imports: [CommonModule, LowerCasePipe],
   templateUrl: './country-selector.component.html',
   styleUrl: './country-selector.component.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: CountrySelectorComponent,
+      multi: true,
+    },
+  ],
 })
-export class CountrySelectorComponent {}
+export class CountrySelectorComponent implements ControlValueAccessor {
+  selected = signal<string>('');
+
+  private onChanged!: (v: string) => void;
+
+  writeValue(value: string): void {
+    this.selected.set(value);
+  }
+
+  registerOnChange(fn: (v: string) => void): void {
+    this.onChanged = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
+  }
+}
